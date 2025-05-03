ced1dade24f397b2dd3fcf5f5f9951a71067eb2f
FlashLON <164888404+FlashLON@users.noreply.github.com>
2025-05-03 16:37:57 +0300

    Update ModHelperData.cs

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -8,6 +8,6 @@ public static class ModHelperData
 
     public const string Description = "An empty mod";
 
-    public const string RepoOwner = ""; // TODO add your github username hero, also in the download url in README.md
-    public const string RepoName = ""; // TODO add your repo name here, also in the download url in README.md
+    public const string RepoOwner = "FlashLON"; // TODO add your github username hero, also in the download url in README.md
+    public const string RepoName = "NatureDruidMod"; // TODO add your repo name here, also in the download url in README.md
 }
